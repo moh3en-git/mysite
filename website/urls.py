a1b1727e6f49239f08bdf3fa989c2fba0a2c317b
moh3en-git <moh3en.cnet@gmail.com>
2022-11-06 20:45:35 +0330

    create website app
    add webssite app to mysite project and handle first request

--- website/urls.py
+++ website/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from .views import *
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('website.urls')),
+  path('', home_view, name='home'),
+   
 
 ]
