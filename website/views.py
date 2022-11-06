54bd3237bebbc3b7378252bfd327720eaf8429b2
moh3en-git <moh3en.cnet@gmail.com>
2022-11-06 21:05:32 +0330

    create temlate pages
    create template page index , contact, about

--- website/views.py
+++ website/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home_view(request):
-    return HttpResponse("<h1> This is a home page </h1>")
+    return render(request,'index.html')
+
+def home_contact(request):
+    return render(request,'contact.html')
+
+def home_about(request):
+    return render(request,'about.html')
