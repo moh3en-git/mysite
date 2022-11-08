558071dccc9fcca50092028193cbd48a902e95ef
moh3en-git <moh3en.cnet@gmail.com>
2022-11-08 21:39:47 +0330

    Transform Template to project
    +index.html
    +about.html
    *contact.html

--- website/views.py
+++ website/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home_view(request):
-    return render(request,'index.html')
+    return render(request,'website\index.html')
 
 def home_contact(request):
-    return render(request,'contact.html')
+    return render(request,'website\contact.html')
 
 def home_about(request):
-    return render(request,'about.html')
+    return render(request,'website\\about.html')
